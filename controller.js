24fd7f1deac17a02f9093356cc7bc28491385ea8
Thaynara yasmin silva carlo <thaynara.carlo@alunoescolassociais.org.br>
2024-08-08 11:56:55 -0300

    Função que edita nomes de lista

--- controller.js
+++ controller.js
@@ -36,8 +36,16 @@ function salvarUser(){
 function criaLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usúario</th><th>Ações</th></tr>";
     for(let i=0;i <= (dadosLista.length - 1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
+
+}
+
+//FUNÇÃO PARA EDITAR NOMES DE LISTA
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista
+    [(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
 
 }
