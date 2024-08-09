dc33a0e101a214ad97bb98cf95cf8e1d2fc56a5a
Thaynara yasmin silva carlo <thaynara.carlo@alunoescolassociais.org.br>
2024-08-09 09:05:53 -0300

    adiciona comentários no código e inmicia no README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# login-cad
+# # Projeto login-cad
+ 
+Este projeto é uma aplicação web simples para cadastrar, listar, editar e excluir nomes de usuários. A página permite que o usuário insira um nome, que é armazenado em uma lista e exibido em uma tabela abaixo. As funções de edição e exclusão estão disponíveis diretamente na tabela.
+ 
+
+
+
+
--- controller.js
+++ controller.js
@@ -1,14 +1,17 @@
-// FUNÇÃO PARA VALIDAÇÃO DE ACESSO 
-
+//  FUNÇÃO INICIAL DE VALIDAÇÃO DE ACESSO 
 function acessar(){
+    // OBTÉM O VALOR DO CAMPO DE E-MAIL
     let loginEmail = document.getElementById 
     ('loginEmail').value;
+    //  OBTÉM O VALOR DO CAMPO DE SENHA 
     let loginSenha = document.getElementById
     ('loginSenha').value;
 
+    // VERIFICA SE ALGUM DOS CAMPOS ESTÁ VAZIO 
     if(!loginEmail || !loginSenha){
         alert("Favor preencher todos os campos");
     }else{
+        // EXIBE UM ALERTA DE SUCESSO SE AMBOS OS CAMPOS ESTIVEREM PRENCHIDOS
         alert("campos preenchidos com sucesso");
         // alert("campos preenchidos com sucesso");
         window.location.href = 'cadastro.html';
@@ -17,7 +20,7 @@ function acessar(){
 
 }
 
-//FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+// FUNÇÃO PARA ADICIONAR NOMES A UM ARRAY E EXIBIR  O ARRAY ATUALIZADO
 var dadosLista = [];
 function salvarUser(){
     let nomeUser = document.getElementById ('nomeUser').value;
@@ -27,25 +30,35 @@ function salvarUser(){
         //console.log(dadosLista);
         criaLista()
         document.getElementById('nomeUser').value = "";
-
     }else{
        alert("Favor informe um nome para cadastro");
     }
 }
-// FUNÇÃO QUE CRIA LISTA DE USUÁRIOS
+
+// FUNÇÃO PARA GERAR UMA TABELA DE USUÁRIOS COM OPÇÕES DE EDIÇÃO E EXCLUSÃO 
 function criaLista(){
+    // INICIALIZA A TABELA COM CABEÇALHOS
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usúario</th><th>Ações</th></tr>";
     for(let i=0;i <= (dadosLista.length - 1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
 
 }
 
-//FUNÇÃO PARA EDITAR NOMES DE LISTA
+//FUNÇÃO PARA EDITAR NOMES NA LISTA DE US
 function editar(i){
-    document.getElementById('nomeUser').value = dadosLista
-    [(i - 1)];
+    // DEFINE O VALOR DO CAMPO 'nomeUser' COM O NOME SELECIONADO 
+    document.getElementById('nomeUser').value = dadosLista [(i - 1)];
+    // REMOVE O NOME SELECIONADO  DA LISTA  DE DADOS
     dadosLista.splice(dadosLista[(i - 1)], 1);
 
+}
+
+/// FUNÇÃO PARA EXCLUIR UM NOME DA LISTA
+function excluir(i){
+    // REMOVE O NOME DO ARRAY DADOSLISTA NO ÍNDICE QUE É FORNECIDO
+    dadosLista.splice((i -1), 1);
+    // ATUALIZA A TABELA PARA REFLETIR A REMOÇÃO
+    document.getElementById('tabela').deleteRow(i)
 }
